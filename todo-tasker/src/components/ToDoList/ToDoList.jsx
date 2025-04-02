dff3a32ed22bbd60fe19c7d29bd80e7a21a47c88
andriesvdw1 <andriesvdw1@gmail.com>
2025-04-02 10:54:21 +0200

    Implement the move task up and down functionality.

--- todo-tasker/src/components/ToDoList/ToDoList.jsx
+++ todo-tasker/src/components/ToDoList/ToDoList.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import './ToDoList.css'
 function ToDoList() {
-    const [tasks, setTasks] = useState(["Wake up", "Drink coffee", "Brush Teeth"]);
+    const [tasks, setTasks] = useState(["Wake up", "Drink coffee", "Brush Teeth","Go to Work"]);
     const [newTask, setNewTask] = useState("");
 
     function handleInputChange(event){
@@ -20,10 +20,20 @@ function ToDoList() {
         setTasks(updatedTasks);
     }
     function moveTaskUp(index){
-
+        if(index > 0){
+            const updatedTasks = [...tasks]; // Create a copy of the current tasks array
+            [updatedTasks[index - 1], updatedTasks[index]] = 
+            [updatedTasks[index], updatedTasks[index - 1]]; // Swap the values of clicked task with above task
+            setTasks(updatedTasks); // Update the tasks state so that tasks show
+        }
     }
     function moveTaskDown(index){
-
+        if(index < tasks.length - 1){ // If not the last task do the swap
+            const updatedTasks = [...tasks]; // Create a copy of the current tasks array
+            [updatedTasks[index], updatedTasks[index + 1]] =
+            [updatedTasks[index + 1], updatedTasks[index]];  // Swap the values of clicked task with below task
+            setTasks(updatedTasks); // Update the tasks state so that tasks show
+        }
     }
 
   return (
